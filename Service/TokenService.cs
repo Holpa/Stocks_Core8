844f96d660647ded42eb1e6daa8ca27f8898eebe
Ahmad Holpa <ahmadholpa@gmail.com>
2024-03-08 14:57:53 -0500

    Claims vs Roles
    Roles are more generic need DB to check all thetime
    Claims dont require DB very flexible
    
    Created Service class and Itoken interface,
    
    to be continued.....

---
+++ Service/TokenService.cs
@@ -0,0 +1,21 @@
+using api.Interfaces;
+using api.Models;
+using Microsoft.IdentityModel.Tokens;
+
+namespace api.Service
+{
+    public class TokenService : ITokenService
+    {
+        private readonly IConfiguration _config;
+        private readonly SymmetricSecurityKey _key;
+        public TokenService(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public string CreateToken(AppUser user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
