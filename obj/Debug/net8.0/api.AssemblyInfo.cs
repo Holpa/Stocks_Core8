7c91c6fec5d6c0f12043bf457217f093b4c08655
Ahmad Holpa <ahmadholpa@gmail.com>
2024-03-12 16:10:14 -0400

    rebuild nothing much

--- obj/Debug/net8.0/api.AssemblyInfo.cs
+++ obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f5e15f08bc52eefa6f1bb2cc16b0cad06a1826b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+855baed97d53c1f407d6bf974121f630593ccc32")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
